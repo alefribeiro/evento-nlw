3fe2f1dcdefb021fbcce6a5c4674f439d5cac0ad
Álef Riebiro <aahribeiro2@gmail.com>
2024-07-09 19:24:23 -0300

    Mastrar quantidade de pessoas convidadas no input de convite

--- src/app.tsx
+++ src/app.tsx
@@ -133,9 +133,15 @@ export function App() {
                 className="flex items-center gap-2 flex-1"
               >
                 <UserRoundPlus className="size-5 text-zinc-400" />
-                <span className="text-lg text-zinc-400 text-left flex-1">
-                  Quem estará na viagem?
-                </span>
+                {emailsToInvite.length > 0 ? (
+                  <span className="text-lg text-zinc-100 text-left flex-1">
+                    {emailsToInvite.length} pessoa(s) convidada(s)
+                  </span>
+                ) : (
+                  <span className="text-lg text-zinc-400 text-left flex-1">
+                    Quem estará na viagem?
+                  </span>
+                )}
               </button>
 
               <div className="w-px h-6 bg-zinc-800" />
